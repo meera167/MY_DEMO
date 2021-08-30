52d1f668264e86e1c121b6707caecc3a997e05ff
meera167 <meeraaln@outlook.com>
2021-08-30 11:30:35 +0400

    my origin file

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'my first demo uplode', ),
+      home: MyHomePage(title: 'my first demo uplode[', ),
     );
   }
 }
